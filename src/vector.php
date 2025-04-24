ff76ddac1b24338f3713a933c420cdec2565204c
Alwin Garside <alwin@garsi.de>
2025-04-24 20:36:05 +0200

    feat: more vector functions

--- src/vector.php
+++ src/vector.php
@@ -13,6 +13,10 @@ namespace empaphy\maphematics\vector;
 
 use empaphy\maphematics\array;
 
+use function array_fill;
+use function array_map;
+use function count;
+
 /**
  * Add two or more vectors together.
  *
@@ -28,12 +32,103 @@ function add(array $vector, array ...$vectors): array
 {
     $vectors = [$vector, ...$vectors];
     $length  = array\check_lengths(...$vectors);
-    $count   = \count($vectors);
-    $result  = \array_fill(0, $length, 0);
+    $result  = array_fill(0, $length, 0);
 
     for ($i = 0; $i < $length; $i++) {
-        for ($j = 0; $j < $count; $j++) {
-            $result[$i] += $vectors[$j][$i];
+        foreach ($vectors as $value) {
+            $result[$i] += $value[$i];
+        }
+    }
+
+    return $result;
+}
+
+/**
+ * Multiplies each component of a vector with the provided scalar.
+ *
+ *       ⎡ x ⎤   ⎡ s⋅x ⎤
+ *     s·⎢ y ⎥ = ⎢ s⋅y ⎥
+ *       ⎣ z ⎦   ⎣ s⋅z ⎦
+ *
+ * @param  array<int|float>  $vector
+ * @param  int|float         $scalar  Value multiplied with each component of
+ *                                    the vector.
+ * @return array<int|float>
+ */
+function scale(array $vector, int | float $scalar): array
+{
+    return array_map(
+        static fn(int | float $c): int | float => $c * $scalar,
+        $vector,
+    );
+}
+
+/**
+ * Divides each component of a vector by the provided scalar.
+ *
+ *     ⎡ x ⎤     ⎡ x÷s ⎤
+ *     ⎢ y ⎥÷s = ⎢ y÷s ⎥
+ *     ⎣ z ⎦     ⎣ z÷s ⎦
+ *
+ * @param  array<int|float>  $vector
+ * @param  int|float         $scalar  Value multiplied with each component of
+ *                                    the vector.
+ * @return array<int|float>
+ */
+function scale_divide(array $vector, int | float $scalar): array
+{
+    return array_map(
+        static fn(int | float $c): int | float => $c / $scalar,
+        $vector,
+    );
+}
+
+/**
+ * Performs a linear transformation on the given vector using a matrix.
+ *
+ * ⎡ x ⎤   ⎡ a  d  g ⎤    ⎡ a ⎤    ⎡ d ⎤    ⎡ g ⎤   ⎡ ax + dy + gz ⎤
+ * ⎢ y ⎥ · ⎢ b  e  h ⎥ = x⎢ b ⎥ + y⎢ e ⎥ + z⎢ h ⎥ = ⎢ bx + ey + hz ⎥
+ * ⎣ z ⎦   ⎣ c  f  i ⎦    ⎣ c ⎦    ⎣ f ⎦    ⎣ i ⎦   ⎣ cx + fy + iz ⎦
+ *
+ * @param  array<int|float>    $vector
+ * @param  array<int|float>[]  $matrix
+ * @return array<int|float>
+ */
+function transform(array $vector, array $matrix): array
+{
+    if (count($matrix[0]) !== count($vector)) {
+        throw new \RangeException('Vector and matrix column count must match.');
+    }
+
+    $v = array_fill(0, count($matrix), 0);
+
+    foreach ($matrix as $i => $row) {
+        foreach ($row as $j => $component) {
+            $v[$i] += $vector[$j] * $component;
+        }
+    }
+
+    return $v;
+}
+
+/**
+ * ⎡ a ⎤   ⎡ x ⎤⎡ .. ⎤   ⎡ a • x • .. ⎤
+ * ⎢ b ⎥ x ⎢ y ⎥⎢ .. ⎥ = ⎢ b • y • .. ⎥
+ * ⎣ c ⎦   ⎣ z ⎦⎣ .. ⎦   ⎣ c • z • .. ⎦
+ *
+ * @param  array<int|float>  ...$vector
+ * @param  array<int|float>  ...$vectors
+ * @return array<int|float>
+ */
+function multiply(array $vector, array ...$vectors): array
+{
+    $vectors = [$vector, ...$vectors];
+    $length  = array\check_lengths(...$vectors);
+    $result  = array_fill(0, $length, 1);
+
+    for ($i = 0; $i < $length; $i++) {
+        foreach ($vectors as $value) {
+            $result[$i] *= $value[$i];
         }
     }
 
--- tests/Unit/VectorTest.php
+++ tests/Unit/VectorTest.php
@@ -8,12 +8,90 @@
 
 use empaphy\maphematics\vector;
 
-describe('vector\add', function () {
-    it('works', function ($vector, $vectors, $expected) {
+describe('vector\\add', function () {
+    test('adds', function ($vector, $vectors, $expected) {
         expect(vector\add($vector, ...$vectors))->toBe($expected);
     })->with([
         'two vectors'         => [[3, -5], [[2, 1]], [3 + 2, -5 + 1]],
         'three vectors'       => [[1, 2], [[3, 5], [7, 11]], [1 + 3 + 7, 2 + 5 + 11]],
         'vectors with floats' => [[3.0, -5.0], [[2.0, 1.0]], [3.0 + 2.0, -5.0 + 1.0]],
+    ]);
+});
+
+describe('vector\\scale', function () {
+    test('scales', function ($vector, $scalar, $expected) {
+        expect(vector\scale($vector, $scalar))->toBe($expected);
+    })->with([
+        'simple scalar' => [[3, -5], 2, [3 * 2, -5 * 2]],
+    ]);
+});
+
+describe('vector\\scale_divide', function () {
+    test('scales and divides', function ($vector, $scalar, $expected) {
+        expect(vector\scale_divide($vector, $scalar))->toBe($expected);
+    })->with([
+        'simple scalar' => [[3, -5], 2, [3 / 2, -5 / 2]],
+    ]);
+});
+
+describe('vector\\transform', function () {
+    test('transforms', function ($vector, $matrix, $expected) {
+        expect(vector\transform($vector, $matrix))->toBe($expected);
+    })->with([
+        [
+            [-1, 2],
+            [
+                [1, 3],
+                [-2, 0],
+            ],
+            [5, 2],
+        ],
+        [
+            [-1, 2],
+            [
+                [1, 3],
+                [-2, 0],
+                [5, 7],
+            ],
+            [5, 2, 9],
+        ],
+        [
+            [-1, 2, 5],
+            [
+                [1, 3, 7],
+                [-2, 0, 11],
+            ],
+            [40, 57],
+        ],
+    ]);
+
+    test("throws RangeException", function ($vector, $matrix) {
+        expect(fn() => vector\transform($vector, $matrix))->toThrow(
+            \RangeException::class,
+            'Vector and matrix column count must match.',
+        );
+    })->with([
+        [
+            [-1, 2, 3],
+            [
+                [1, 3],
+                [-2, 0],
+            ],
+        ],
+        [
+            [-1, 2, 3],
+            [
+                [1, 3],
+                [-2, 0],
+                [5, 7],
+            ],
+        ],
+        [
+            [-1, 2],
+            [
+                [1, 3, 5],
+                [-2, 0, 7],
+            ],
+        ],
     ]);
 });
